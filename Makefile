4e29b103e499a327c3c7bb3033202f5ebbdb16e5
RuthlessLion <windowtheworldofgames@gmail.com>
2017-04-29 10:45:50 +0700

    Hot fix

--- Makefile
+++ Makefile
@@ -1,18 +1,2 @@
-GCC       =  g++
-FLAGS	  =  -Wall
-SFMLFLAGS =  -lsfml-graphics -lsfml-window -lsfml-system
-APPNAME   =  application
-
-all: app
-
-clean:
-	rm -rf *.o
-
-main.o: main.cpp
-	$(GCC) -c -o main.o main.cpp
-
-app: main.o
-	$(GCC) $(SFMLFLAGS) -o $(APPNAME) main.o 
-
-run: $(APPNAME)
-	 ./$(APPNAME)
+all:
+	g++ main.cpp -o main.o -Wall -lsfml-graphics -lsfml-window -lsfml-system
--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
-#include "main.hpp"
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "data/scene.cpp"
+// #include "main.hpp"
 
 int main() {
     sf::RenderWindow app(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "KeyboardNinja", sf::Style::Titlebar | sf::Style::Close);
     app.setFramerateLimit(60);
 
     //Инициализация сцен
-    initAllScenes(app);
+    // initAllScenes(app);
+
+    scene_main_menu = new kb::SceneMainMenu;
+    scene_main_menu->init(&app);
+
+    scene_game = new kb::SceneGame;
+    scene_game->init(&app);
+
+    scene_table_lead = new kb::SceneTableLead;
+    scene_table_lead->init(&app);
+
+    scene_input_lead = new kb::SceneInputLead;
+    scene_input_lead->init(&app);
+
     scene = scene_main_menu;
 
     while (app.isOpen())
@@ -20,17 +37,7 @@ int main() {
     return 0;
 }
 
-int initAllScenes(sf::RenderWindow &app) {  // Рома, доволен? Адрес - не указатель )0))
-    scene_main_menu = new kb::SceneMainMenu;
-    scene_main_menu->init(&app);
-
-    scene_game = new kb::SceneGame;
-    scene_game->init(&app);
-
-    scene_table_lead = new kb::SceneTableLead;
-    scene_table_lead->init(&app);
-
-    scene_input_lead = new kb::SceneInputLead;
-    scene_input_lead->init(&app);
+/*int initAllScenes(sf::RenderWindow &app) {  // Рома, доволен? Адрес - не указатель )0))
+    
     return 0;
-}
+}*/
